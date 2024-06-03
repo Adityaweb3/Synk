67969d3d194e246088fb998c1018ce530cf1155e
adityaweb3 <ap7596112@gmail.com>
2024-06-03 19:47:47 +0530

    handling post request for login

--- users/views.py
+++ users/views.py
@@ -1,7 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm 
 # Create your views here.
+from django.contrib.auth import authenticate,login
 
 def user_login(request) : 
-    form = LoginForm()
-    return render(request , 'users/login.html' , {'form' : form})
+
+    if request.method=="POST" :
+        form = LoginForm(request.POST)
+        if form.is_valid() :
+            data = form.cleaned_data
+            user=authenticate(request , username=data['username'] , password = data['password'])
+            if user is not None : 
+                login(request , user)
+                return HttpResponse("user logged in successfully")
+            
+            else :
+                return HttpResponse("Invalid Credentials")
+
+
+
+
+
+    else :
+        form = LoginForm()
+        return render(request , 'users/login.html' , {'form' : form})
