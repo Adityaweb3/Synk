7e34378d3bc255934df366ea679a9b539730c944
adityaweb3 <ap7596112@gmail.com>
2024-09-10 14:24:20 +0530

    added password change view and template

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.user_login , name='login'),
     path('' , views.index , name ='index') , 
     path('logout/', auth_view.LogoutView.as_view(http_method_names=["post", "get", "options"], template_name='users/logout.html') , name='logout'),
+    path('password_change/' , auth_view.PasswordChangeView.as_view(template_name='users/password_change_form.html') , name='password_change'),
+    path('password_change_done/' , auth_view.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html') , name='password_change_done'),
 ]
