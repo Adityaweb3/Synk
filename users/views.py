b368bbd04d13df190275b4718a72954b56ae8ea0
adityaweb3 <ap7596112@gmail.com>
2024-09-10 20:00:39 +0530

    register user functionality

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm 
+from .forms import LoginForm, UserRegistrationForm
 # Create your views here.
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.decorators import login_required 
@@ -26,3 +26,19 @@ def user_login(request) :
 @login_required
 def index(request) : 
     return render(request , 'users/index.html')
+
+
+def register(request) : 
+    if request.method=='POST' : 
+        user_form=UserRegistrationForm(request.POST)
+        if user_form.is_valid() : 
+            new_user= user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request , 'users/register_done.html')
+    else :
+        user_form = UserRegistrationForm()
+    return render(request , 'users/register.html' , {'user_form':user_form})
+
+
+        
