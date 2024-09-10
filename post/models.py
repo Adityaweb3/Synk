de2f7aae0a4319d943c823a1d6d73dc1922bd697
adityaweb3 <ap7596112@gmail.com>
2024-09-10 22:51:25 +0530

    making migrations for post

--- post/models.py
+++ post/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings 
 # Create your models here.
+from django.utils.text import slugify
 
 class Post (models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
@@ -8,8 +9,12 @@ class Post (models.Model):
     caption=models.TextField(blank=True)
     title=models.CharField(max_length=200)
     slug = models.SlugField(max_length=200 , blank=True)
-    created = models.DateField(auto_new_add=True)
+    created = models.DateField(auto_now_add=True)
 
     def __str__(self) : 
         return self.title
-
+    
+    def save(self , *args , **kwargs) : 
+        if not self.slug : 
+            self.slug= slugify(self.title)
+        super().save(*args, **kwargs)
