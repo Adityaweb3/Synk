25faef16613c86ac57a45ae73a463892d661468a
adityaweb3 <ap7596112@gmail.com>
2024-09-10 14:39:12 +0530

    password reset done view

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('' , views.index , name ='index') , 
     path('logout/', auth_view.LogoutView.as_view(http_method_names=["post", "get", "options"], template_name='users/logout.html') , name='logout'),
     path('password_change/' , auth_view.PasswordChangeView.as_view(template_name='users/password_change_form.html') , name='password_change'),
-    path('password_change_done/' , auth_view.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html') , name='password_change_done'),
-    path('password_reset' , auth_view.PasswordResetView.as_view(template_name='users/password_reset_form.html') , name='password_reset'),
+    path('password_change/done/' , auth_view.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html') , name='password_change_done'),
+    path('password_reset/' , auth_view.PasswordResetView.as_view(template_name='users/password_reset_form.html') , name='password_reset'),
+    path('password_reset/done/' , auth_view.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html') , name='password_reset_done'),
 ]
